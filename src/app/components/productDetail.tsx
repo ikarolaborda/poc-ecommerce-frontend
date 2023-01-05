c9e8d494ed3a7d88f0c434ae57050bee5ee7b999
Ikaro C. Laborda <iclaborda@msn.com>
2023-01-05 18:15:53 +0000

    feat: designing productDetail in material UI perspective

--- src/app/components/productDetail.tsx
+++ src/app/components/productDetail.tsx
@@ -1,12 +1,23 @@
 import {Product} from "../models/Product";
 import {Link, useParams} from "react-router-dom";
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import {ProductService} from "../services/productService";
+import {
+    Button,
+    Divider,
+    Grid,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableRow,
+    Typography
+} from "@mui/material";
 
 
 export default function ProductDetail() {
     const {id} = useParams<{id: string}>();
-    const [product, setProduct] = useState<Product | null >();
+    const [product, setProduct] = useState<Product | null>(null);
     const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
@@ -23,30 +34,61 @@ export default function ProductDetail() {
         return <div>Loading...</div>;
     }
 
+    if(!product) {
+        return <h4>Product not found</h4>;
+    }
+
     return (
-        <div className="container mt-5">
-            <div className="row">
-                <div className="col-md-12 d-flex">
-                    <div className="col-md-6">
-                        <img src={product!.pictureUrl} className="img-fluid" alt="product_picture"/>
-                    </div>
-                    <div className="col-md-6">
-                        <div className="card w-100">
-                            <div className="card-header">
-                                <h4 className="card-title">{product!.name}</h4>
-                            </div>
-                            <div className="card-body">
-                                <p className="card-text">{product!.description}</p>
-                                <p className="card-text">Quantity in Stock: {product!.quantityInStock}</p>
-                                <p className="card-text">Price: {product!.price}</p>
-                            </div>
-                            <div className="card-footer">
-                                <Link to="/catalog" className="btn btn-warning">Back to Catalog</Link>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <>
+            <Grid container spacing={6}>
+                <Grid item xs={6} sm={12} md={6} lg={6}>
+                    <img src={product.pictureUrl} alt={product.name} style={{ width: '100%' }}/>
+                </Grid>
+                <Grid item xs={6} sm={12} md={6} lg={6}>
+                    <Typography variant="h3" gutterBottom>{product.name}</Typography>
+                    <Divider sx={{ mb: 2 }}/>
+                    <Typography variant="h4" color='secondary' gutterBottom>R$ {(product.price / 10).toFixed(2)}</Typography>
+                    <TableContainer>
+                        <Table>
+                            <TableBody>
+                                <TableRow>
+                                    <TableCell>
+                                        Name
+                                    </TableCell>
+                                    <TableCell>
+                                        {product.name}
+                                    </TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell>
+                                        Brand
+                                    </TableCell>
+                                    <TableCell>
+                                        {product.productBrand}
+                                    </TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell>
+                                        Description
+                                    </TableCell>
+                                    <TableCell>
+                                        {product.description}
+                                    </TableCell>
+                                </TableRow>
+                                <TableRow>
+                                    <TableCell>
+                                        Type
+                                    </TableCell>
+                                    <TableCell>
+                                        {product.productType}
+                                    </TableCell>
+                                </TableRow>
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                    <Button component={Link} to={'/catalog'} sx={{ mt: 2, typography: 'h6' }}>Back to catalog</Button>
+                </Grid>
+            </Grid>
+        </>
     );
 }
